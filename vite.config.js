720988cf2caec51a7eb3e22167036a31138702b4
NicolasCh25 <nicolas.chiguano@epn.edu.ec>
2025-11-30 21:24:21 -0500

    Actualizacion

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,21 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // 🔥 Agregado para permitir Railway
+  server: {
+    host: true,
+    strictPort: true,
+    allowedHosts: [
+      "front-tesis-production.up.railway.app"
+    ]
+  },
+
+  preview: {
+    host: true,
+    strictPort: true,
+    allowedHosts: [
+      "front-tesis-production.up.railway.app"
+    ]
+  }
 });
